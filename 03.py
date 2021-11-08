49de01faa1b34280d9a1535dedd73fe9e2081289
Saulo Cavalcante <saulocavalcante25@hotmail.com>
2021-11-08 13:53:42 -0300

    adds q3

---
+++ 03.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+x = 0.1
+y = '0. '
+
+for i in range (10):
+    x = x * 2
+    if int(x) == 0:
+        y = y + '0'
+    else:
+        y = y + '1'
+        x = x - 1
+    print(y)
